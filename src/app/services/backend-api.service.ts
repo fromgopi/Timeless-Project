d254c05453ce3b82af8cfb4f0e9d1e7f5f271fc4
MUKTEVI GOPI KRISHNA <muktevigk@handbookinc.com>
2019-04-14 23:24:46 +0530

    added some tables

--- src/app/services/backend-api.service.ts
+++ src/app/services/backend-api.service.ts
@@ -1,15 +1,25 @@
 import { Injectable } from '@angular/core';
 import {from, Observable} from 'rxjs';
-import {HttpClient} from '@angular/common/http';
-import {map} from 'rxjs/operators';
+import {HttpClient, HttpHeaders} from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class BackendApiService {
 
+  private baseUrl = 'https://api.github.com/users';
 
   constructor(private http: HttpClient) { }
+
+  getAll(username: string): Observable<any> {
+    console.log(this.baseUrl + '/' + username['githubUserName'] + '/repos');
+
+
+    const headers = new HttpHeaders({
+    });
+
+    return this.http.get(this.baseUrl + '/' + username['githubUserName'] + '/repos', {headers: headers});
+  }
 
   getAgentLog(): Observable<any> {
     return this.http.get('http://0.0.0.0:5000/data');
